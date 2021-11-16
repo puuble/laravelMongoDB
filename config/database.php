4899fe012c4631c13479576c955df28c11f527aa
marian ivanov <puubles@gmail.com>
2021-11-17 00:27:11 +0200

    add dsn
    dsn for connect easy way

--- config/database.php
+++ config/database.php
@@ -63,19 +63,10 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-        'mongodb' => [
+       'mongodb' => [
             'driver' => 'mongodb',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE'),
-            'username' => env('DB_USERNAME'),
-            'password' => env('DB_PASSWORD'),
-            'options' => [
-                // here you can pass more settings to the Mongo Driver Manager
-                // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
-
-                'database' =>  env('DB_DATABASE')
-            ],
+            'dsn' => env('DB_DSN'),
+            'database' => env('DB_DATABASE', 'homestead'),
         ],
         'pgsql' => [
             'driver' => 'pgsql',
